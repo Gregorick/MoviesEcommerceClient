9f1d44d7d3b64d9722428419958c22478a482ed4
Gregorick <gregorick.liriano@gmail.com>
2022-07-14 00:58:38 -0400

    Mostrar direcciones disponibles en el carrito

--- components/Cart/AddressShipping/AddressShipping.js
+++ components/Cart/AddressShipping/AddressShipping.js
@@ -18,8 +18,40 @@ export default function AddressShipping() {
   }, []);
 
   return (
-    <div>
-      <h1>AddressShipping</h1>
+    <div class="address-shipping">
+      <div className="title">Dirección de envío</div>
+      <div className="data">
+        {size(addresses) === 0 ? (
+          <h3>
+            No hay ninguna dirección creada{" "}
+            <Link href="/account">
+              <a>añade tu primera dirección</a>
+            </Link>
+          </h3>
+        ) : (
+          <Grid>
+            {map(addresses, (address) => (
+              <Grid.Column key={address.id} mobile={16} tablet={8} computer={4}>
+                <Address address={address} />
+              </Grid.Column>
+            ))}
+          </Grid>
+        )}
+      </div>
     </div>
   );
 }
+
+const Address = ({ address }) => {
+  return (
+    <div className="address">
+      <p>{address.title}</p>
+      <p>{address.name}</p>
+      <p>{address.address}</p>
+      <p>
+        {address.city}, {address.state} {address.postalCode}
+      </p>
+      <p>{address.phone}</p>
+    </div>
+  );
+};
